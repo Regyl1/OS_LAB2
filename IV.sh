9e60a547e69d6e0da18732e701938037daacab8a
Regyl1 <roma.orsha@mail.ru>
2020-10-15 13:30:39 +0300

    v0.3

---
+++ IV.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+for pid in $(ps -A o pid)
+do
+
+ppid=$(grep -s "PPid" "/proc/"$pid"/status" | grep -E -o "[0-9]+")
+sumExecRuntime=$(grep -s "sum_exec_runtime" "/proc/"$pid"/sched" | grep -E -o "[0-9]+(.[0-9]+)?")
+nrSwithes=$(grep -s "nr_switches" "/proc/"$pid"/sched" | grep -E -o "[0-9]+")
+
+if [[ -n $nrSwithes ]]
+then
+art=$(bc <<< "$sumExecRuntime/$nrSwithes")
+echo "ProcessID=$pid : Parent_ProcessID=$ppid : Average_Running_Time=$art"
+fi
+done | sort -n -t = -k 3 > ansIV
+
