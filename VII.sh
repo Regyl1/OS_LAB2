6c4ae3e3f6a47e862e940397141dcc64bd09227d
Regyl1 <roma.orsha@mail.ru>
2020-10-16 12:51:26 +0300

    v0.6

---
+++ VII.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+for i in $(ps -A o pid,command | awk '{print $1":"$2}')
+do
+pid=$(echo $i | cut -d ":" -f 1)
+cmd=$(echo $i | cut -d ":" -f 2)
+pastReadBytes=$(grep -s "read_bytes" "/proc/"$pid"/io" | grep -E -o "[0-9]+")
+if [[ -n $pastReadBytes ]]
+then
+echo "$pid $cmd $pastReadBytes"
+fi
+done > "tempVII"
+
+for i in $(ps -A o pid,command | awk '{print $1":"$2}' )
+do
+pid=$(echo $i | cut -d ":" -f 1)
+cmd=$(echo $i | cut -d ":" -f 2)
+newReadBytes=$(grep -s "read_bytes" "/proc/"$pid"/io" | grep -E -o "[0-9]+")
+pastReadBytes=$(cat "tempVII" | grep "$pid" | awk '{print $3}')
+if [[ -n $pastReadBytes && -n $newReadBytes ]]
+then
+readBytes=$(($newReadBytes - $pastReadBytes))
+echo "PID:$pid CMD:$cmd BYTES:$(( $readBytes ))"
+fi
+done | sort -n -t ':' -k 3 | head -3
+
+rm "tempVII"
